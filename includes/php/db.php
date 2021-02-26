de5a8855ccdb6551b156c4030921325f1d487350
pez <emvenegas@gmail.com>
2021-02-26 20:48:50 +0000

    added bootstrap and db config file

---
+++ includes/php/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    'my*8-9+6POiusql',
+    'companydirectory'
+);
+
+
+/*      checking connection to data base
+if (isset($conn)) {
+    echo 'DB conectada';
+}
+*/
+
+
+?>
